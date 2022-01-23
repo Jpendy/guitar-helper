bce89ce493493e3b049c9535c5d9d7da988ab303
Jpendy <Jpendy256@gmail.com>
2022-01-23 12:36:45 -0800

    woooo

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -9,7 +9,7 @@ export default function Guitar() {
     // const aMinorPentatonic = ['A', 'C', 'D', 'E', 'G']
 
     const [notes, setNotes] = useState(cMajorScale)
-    const [root, setRoot] = useState('')
+    const [root, setRoot] = useState('C')
     const [showAllNotes, setShowAllNotes] = useState(false)
     const [reverse, setReverse] = useState(false)
 
@@ -21,15 +21,21 @@ export default function Guitar() {
         setReverse(prev => !prev)
     }
 
+    const handleChangeNotes = (e) => {
+        setNotes(e.target.value.split(/(\s|,)/).map(note => note.toUpperCase()))
+        setRoot('')
+    }
+
     return (
         <>
             <div>
-                <input onChange={e => setRoot(e.target.value[0].toUpperCase())} placeholder="Root Note" />
-                <input onChange={e => setNotes(e.target.value.split(/(\s|,)/).map(note => note.toUpperCase()))} placeholder={notes} />
+                <input onChange={e => setRoot(e.target.value[0].toUpperCase())} placeholder={root || "Root Note"} />
+                <input onChange={handleChangeNotes} placeholder={notes} />
                 <button onClick={toggleShowAllNotes} style={{ height: '21.5px', width: '130px', cursor: 'pointer' }} >{showAllNotes ? 'Hide Other Notes' : 'Show All Notes'}</button>
                 <button onClick={toggleReverse} style={{ height: '21.5px', width: '130px', cursor: 'pointer' }} >{reverse ? 'Switch to righty' : 'Switch to lefty'}</button>
             </div>
             <div>
+                <h2>Root Note: {root}</h2>
                 <h2>Notes Shown: {notes}</h2>
             </div>
             <FretBoard
