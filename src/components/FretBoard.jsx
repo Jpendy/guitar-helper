cee3a7f8b18c431760887da35e2a5518925c5dba
Jpendy <Jpendy256@gmail.com>
2022-02-05 21:41:23 -0800

    commit

--- src/components/FretBoard.jsx
+++ src/components/FretBoard.jsx
@@ -52,31 +52,30 @@ export default function FretBoard({ root, notes, showAllNotes, reverse }) {
 
     const markersArr = [...Array(25)]
 
-    const fretWidth = (i, border = true) => {
-        // if (!border) {
-        //     const extra = 3.5;
-        //     return reverse ? `${(i * 3) + 35 + extra}px` : `${107 - (i * 3) + extra}px`;
-        // }
-        return reverse ? `${(i * 3) + 35}px` : `${107 - (i * 3)}px`;
-    }
+    const fretWidth = (i, border = true) => reverse ? `${(i * 3) + 35}px` : `${107 - (i * 3)}px`;
 
     return (
         <main style={{ marginTop: '20px' }} >
             {fretBoard.map(string => (
                 <div style={{ display: 'flex', width: '100%' }}>
-                    {string.map((note, i) => (
-                        <div style={{
-                            backgroundColor: note === root ? 'lightskyblue' : (notes.includes(note) && 'lawngreen'),
-                            height: '20px',
-                            width: fretWidth(i),
-                            border: reverse ? (i !== string.length - 1 && '1px solid black') : (i !== 0 && '1px solid black'),
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }} >
-                            {notes.includes(note) ? note : showAllNotes ? note : reverse ? (i === string.length - 1 ? note : '') : (i === 0 ? note : '')}
-                        </div>
-                    ))}
+                    {string.map((note, i) => {
+
+                        const isNote = notes.find(item => item.note === note)
+                        return (
+                            <div style={{
+                                backgroundColor: note === root ? 'lightskyblue' : (isNote?.color),
+                                height: '20px',
+                                width: fretWidth(i),
+                                border: reverse ? (i !== string.length - 1 && '1px solid black') : (i !== 0 && '1px solid black'),
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }} >
+                                {isNote ? note : showAllNotes ? note : reverse ? (i === string.length - 1 ? note : '') : (i === 0 ? note : '')}
+                            </div>
+                        )
+                    }
+                    )}
                 </div>
             ))
             }
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -8,7 +8,7 @@ export default function Guitar() {
     const cMajorScale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
     // const aMinorPentatonic = ['A', 'C', 'D', 'E', 'G']
 
-    const [notes, setNotes] = useState(cMajorScale)
+    const [notes, setNotes] = useState(cMajorScale.map(note => ({ note, color: '#7cfc00' })))
     const [root, setRoot] = useState('C')
     const [showAllNotes, setShowAllNotes] = useState(false)
     const [reverse, setReverse] = useState(false)
@@ -22,21 +22,52 @@ export default function Guitar() {
     }
 
     const handleChangeNotes = (e) => {
-        setNotes(e.target.value.split(/(\s|,)/).map(note => note.toUpperCase()))
+        const newNotes = e.target.value.split(/(\s|,)/).map(note => note.toUpperCase())
+        setNotes(prev => prev.map(note => {
+            if (newNotes.includes[note.note])
+        }))
         setRoot('')
+    }
+
+    const handleNoteColorChange = (e) => {
+        setNotes(prev => {
+            return prev.map(note => {
+                if (note.note === e.target.name) {
+                    return {
+                        note: note.note,
+                        color: e.target.value
+                    }
+                }
+                return note
+            })
+        })
     }
 
     return (
         <>
             <div>
                 <input onChange={e => setRoot(e.target.value[0].toUpperCase())} placeholder={root || "Root Note"} />
-                <input onChange={handleChangeNotes} placeholder={notes} />
+                <input onChange={handleChangeNotes} placeholder={notes.map(note => note.note)} />
                 <button onClick={toggleShowAllNotes} style={{ height: '21.5px', width: '130px', cursor: 'pointer' }} >{showAllNotes ? 'Hide Other Notes' : 'Show All Notes'}</button>
                 <button onClick={toggleReverse} style={{ height: '21.5px', width: '130px', cursor: 'pointer' }} >{reverse ? 'Switch to righty' : 'Switch to lefty'}</button>
             </div>
             <div>
                 <h2>Root Note: {root}</h2>
-                <h2>Notes Shown: {notes}</h2>
+                <h2>Notes Shown: {notes.map(note => (
+                    <label
+                        key={note.note}
+                        htmlFor={`${note.note}-${note.color}`}
+                    >
+                        {' '}{note.note}:
+                        <input
+                            id={`${note.note}-${note.color}`}
+                            type="color"
+                            name={note.note}
+                            value={note.color}
+                            onChange={handleNoteColorChange}
+                        />
+                    </label>
+                ))}</h2>
             </div>
             <FretBoard
                 root={root}
